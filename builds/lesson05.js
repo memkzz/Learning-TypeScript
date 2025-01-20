059c798676be99d88ecae03ec0ed3ea5f9b70837
Emerson Ferreira <sfr.emerson@gmail.com>
2025-01-20 01:10:18 -0300

    Union Types

---
+++ builds/lesson05.js
@@ -0,0 +1,10 @@
+"use strict";
+let vtest;
+vtest = "Jestt";
+let courses = ["Javascipt", "TypeScipt", "Java"];
+let values = [11, 22, 33, 44];
+courses.push("Arduino");
+values.push(55, 66, 77);
+console.log(courses);
+console.log(values);
+console.log(vtest);
---
+++ src/lesson05.ts
@@ -0,0 +1,15 @@
+let vtest:string|number;
+vtest = "Jestt";
+
+let courses = ["Javascipt", "TypeScipt", "Java"];
+let values = [11,22,33,44];
+
+courses.push("Arduino");
+values.push(55,66,77);
+
+
+
+
+console.log(courses);
+console.log(values);
+console.log(vtest);
