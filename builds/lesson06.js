8fcecb5dad19a08e281ea32d07ccc509baa19ed0
Emerson Ferreira <sfr.emerson@gmail.com>
2025-01-20 14:15:11 -0300

    Array and Readonly Array

---
+++ builds/lesson06.js
@@ -0,0 +1,7 @@
+var numero = [11, 22, 33];
+numero.push(23);
+console.log(numero);
+var number_ro;
+3, 2;
+;
+console.log(number_ro);
---
+++ src/lesson06.ts
@@ -0,0 +1,8 @@
+let numero = [11, 22, 33];
+numero.push(23);
+
+
+console.log(numero);
+
+let number_ro:ReadonlyArray<number>[23,3,2];
+console.log(number_ro);
